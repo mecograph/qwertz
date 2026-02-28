c609eab062da070b78ca47675090f6fce0613394
Tobias Wolfram <t.wolfram@cubemos.com>
2026-03-01 00:29:10 +0100

    chore: enable HTTPS for Vite dev server

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), basicSsl()],
 });
